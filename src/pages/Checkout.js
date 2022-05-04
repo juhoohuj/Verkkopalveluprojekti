734f88d3cfd62a82a78442ae816c34cd601c28cb
Jussi Kujala <jussijustus@gmail.com>
2022-05-04 18:20:02 +0300

    checkout sivulta poistettu keskeneräinen tuotelista

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -53,36 +53,6 @@ export default function Checkout({URL, cart, emptyCart}) {
 <div className="maincontainer">
         <div className="container">
           <div className="row">
-            <div className="col-md-4 order-md-2 mb-4">
-              <h4 className="d-flex justify-content-between align-items-center mb-3">
-                <span className="text-muted">Ostoskorisi</span>
-                <span className="badge badge-secondary badge-pill">3</span>
-              </h4>
-              <ul className="list-group mb-3">
-                <li className="list-group-item d-flex justify-content-between lh-condensed">
-                  <div>
-                    <h6 className="my-0">Product name</h6>
-                  </div>
-                  <span className="text-muted">12€</span>
-                </li>
-                <li className="list-group-item d-flex justify-content-between lh-condensed">
-                  <div>
-                    <h6 className="my-0">Second product</h6>
-                  </div>
-                  <span className="text-muted">8€</span>
-                </li>
-                <li className="list-group-item d-flex justify-content-between lh-condensed">
-                  <div>
-                    <h6 className="my-0">Third item</h6>
-                  </div>
-                  <span className="text-muted">5€</span>
-                </li>
-                <li className="list-group-item d-flex justify-content-between">
-                  <span>Yhteensä</span>
-                  <strong>25€</strong>
-                </li>
-              </ul>
-            </div>
             <div className="col-md-8 order-md-1">
               <h4 className="mb-3">Toimitustiedot</h4>
               <form className="needs-validation" noValidate>
